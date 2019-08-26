bd7d83433a05e55257b2334a10c510b935ad2d07
timothy-tianmu <tianmu_19970924@163.com>
2019-08-27 00:07:53 +0800

    setup project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface IAnimal {
+    name: string;
+    age: number;
+}
+
+class Dog implements IAnimal {
+    public name: string = "dog";
+    public age: number = 12;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+let d = new Dog("D", 12);
