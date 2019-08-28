e090cf0833ec143cae78aa1b02c8f8d15c450eae
timothy-tianmu <tianmu_19970924@163.com>
2019-08-28 21:42:44 +0800

    edit: tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-interface IAnimal {
-    name: string;
-    age: number;
+class Parser {
+    public tokens = ["a", "b", "c"];
+
 }
 
-class Dog implements IAnimal {
-    public name: string = "dog";
-    public age: number = 12;
-    constructor(name: string, age: number) {
-        this.name = name;
-        this.age = age;
-    }
-}
-
-let d = new Dog("D", 12);
-console.log("name: " + d.name);
-console.log("age: " + d.age);
+let p = new Parser()
+console.log(p.tokens.map((t: string) => t+"d"))
